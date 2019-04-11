2884f3a44d9d59c3c90dea611bcd53de695c4c4e
Ayush Mittal - User <ayushmittal-user@dhcp-rhodes-402.redrover.cornell.edu>
2019-04-11 16:13:51 -0400

    new backend changes

--- routes/api.php
+++ routes/api.php
@@ -235,6 +235,108 @@ Route::put('/updateResource', function(Request $request) {
     }  
 });
 
+/** Route that updates an entry to the resources_per_projects table
+ * ResourceID, ProjectID, ScheduleID: auto-incrementing key, so value that is inputted for it does not matter 
+
+{
+    "ProjectName": "P2",
+    "NetID": "jd111",
+    "Role": "Product Manager"
+}
+
+ */
+Route::put('/updateResourcePerProject', function(Request $request) {
+    $data = $request->all();
+    
+    $project_id_array = DB::table('projects')->select('ProjectID')->where('ProjectName', '=', $data["ProjectName"])->get();
+    $project_id_json = json_decode(json_encode($project_id_array{0}), true);
+    $project_id = $project_id_json["ProjectID"];
+
+    $resource_id_array = DB::table('resources')->select('ResourceID')->where('NetID', '=', $data["NetID"])->get();
+    $resource_id_json = json_decode(json_encode($resource_id_array{0}), true);
+    $resource_id = $resource_id_json["ResourceID"];
+
+    try {
+        DB::table('resources_per_projects')->where([['ProjectID', $project_id], ['ResourceID', $resource_id]])->update(
+        ["Role" => $data["Role"]]);
+        return "Successfully Updated Existing ResourcePerProject";
+    } catch (Exception $e){
+        echo($e->getMessage());
+        $error_code = $e->errorInfo[1];
+        if($error_code == 1062){
+            return 'This resource already exists on this project';
+        } else {
+            return 'This resource could not be updated. Please try again.';
+        }
+    }  
+});
+
+/** Route that updates an entry to the schedules table
+ * ScheduleID: auto-incrementing key, so value that is inputted for it does not matter 
+
+{
+    "ProjectName": "P2",
+    "NetID": "jd111",
+    "Dates": "2019-03-07",
+    "HoursPerWeek": 30
+}
+
+ */
+Route::put('/updateSchedule', function(Request $request) {
+    $data = $request->all();
+    
+    $project_id_array = DB::table('projects')->select('ProjectID')->where('ProjectName', '=', $data["ProjectName"])->get();
+    $project_id_json = json_decode(json_encode($project_id_array{0}), true);
+    $project_id = $project_id_json["ProjectID"];
+
+    $resource_id_array = DB::table('resources')->select('ResourceID')->where('NetID', '=', $data["NetID"])->get();
+    $resource_id_json = json_decode(json_encode($resource_id_array{0}), true);
+    $resource_id = $resource_id_json["ResourceID"];
+
+    $schedule_id_array = DB::table('resources_per_projects')->select('ScheduleID')->where([['ProjectID', '=', $project_id], ['ResourceID', '=', $resource_id]])->get();
+    $schedule_id_json = json_decode(json_encode($schedule_id_array{0}), true);
+    $schedule_id = $schedule_id_json["ScheduleID"];
+
+    try {
+        DB::table('schedules')->where('ScheduleID', $schedule_id)->update(
+        ["Dates" => $data["Dates"], "HoursPerWeek" => $data["HoursPerWeek"]]);
+        return "Successfully Updated Existing Schedule";
+    } catch (Exception $e){
+        echo($e->getMessage());
+        $error_code = $e->errorInfo[1];
+        if($error_code == 1062){
+            return 'This schedule already exists';
+        } else {
+            return 'This resource could not be updated. Please try again.';
+        }
+    }  
+});
+
+/** Route that deletes a  project in the projects table 
+ * ProjectID: auto-incrementing key, so value that is inputted for it does not matter 
+
+{
+    "ProjectName": "P2",
+}
+
+*/
+Route::delete("/deleteProject", function(Request $request) {
+    $data = $request->all();
+
+    try {
+        DB::table('projects')->where('ProjectName', '=', $data["ProjectName"])->delete();
+        return "Successfully Deleted Existing Project";
+    } catch (Exception $e){
+        echo($e->getMessage());
+        $error_code = $e->errorInfo[1];
+        if($error_code == 1062){
+            return 'A project does not exist with this name';
+        } else {
+            return 'This project could not be deleted. Please try again.';
+        }
+    }
+});
+
 /** Route that clears all records from all tables */
 // Route::get('/clear', function() {
 //     DB::table('schedules')->delete();
