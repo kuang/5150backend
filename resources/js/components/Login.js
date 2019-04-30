beec54e3e39b926d329c25c70a5fa03783245d31
liang195 <liang195@umn.edu>
2019-04-30 13:53:25 -0400

    add logout button

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import GoogleLogin from 'react-google-login';
-import GoogleLogout from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      direct: false
+      direct: false,
+      user: null
     }
   }
 
@@ -17,14 +18,24 @@ class Login extends React.Component {
       console.log(response);
     }
 
+    const logout = (response) => {
+      console.log("You are logged out");
+    }
+
     return (
-      <GoogleLogin
-        clientId="795086897508-p73emkkcd287sf6e4nm8jgb45susbcg1.apps.googleusercontent.com"
-        buttonText="Login With Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
+      <div>
+        <GoogleLogin
+          clientId="795086897508-p73emkkcd287sf6e4nm8jgb45susbcg1.apps.googleusercontent.com"
+          buttonText="Login With Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+        <GoogleLogout
+          buttonText="Logout"
+          onLogoutSuccess={logout}
+        ></GoogleLogout>
+      </div>
     );
   }
 }
