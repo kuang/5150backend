586245e7e69fbd7c01db570f51e8f7dcec9ffd7f
liang195 <liang195@umn.edu>
2019-04-30 15:56:32 -0400

    add div to login/logout button for style

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -9,32 +9,49 @@ class Login extends React.Component {
     super(props);
     this.state = {
       direct: false,
-      user: null
-    }
+      user: "",
+      login: false
+    };
+    this.loginSuccess = this.loginSuccess.bind(this);
+    this.loginError = this.loginError.bind(this);
+    this.logoutResponse = this.logoutResponse.bind(this);
+  }
+
+  loginSuccess(response) {
+    console.log(response);
+    this.setState({
+      user: response.profileObj.name,
+      login: true
+    });
+  }
+
+  loginError(response) {
+    console.log(response);
+  }
+
+  logoutResponse(response) {
+    console.log("You are logged out");
   }
 
   render() {
-    const responseGoogle = (response) => {
-      console.log(response);
-    }
-
-    const logout = (response) => {
-      console.log("You are logged out");
-    }
-
     return (
       <div>
-        <GoogleLogin
-          clientId="795086897508-p73emkkcd287sf6e4nm8jgb45susbcg1.apps.googleusercontent.com"
-          buttonText="Login With Google"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-        />
-        <GoogleLogout
-          buttonText="Logout"
-          onLogoutSuccess={logout}
-        ></GoogleLogout>
+        <div id='login-button'>
+          <GoogleLogin
+            clientId="795086897508-p73emkkcd287sf6e4nm8jgb45susbcg1.apps.googleusercontent.com"
+            buttonText="Login With Google"
+            onSuccess={this.loginSuccess}
+            onFailure={this.loginError}
+            cookiePolicy={'single_host_origin'}
+          />
+        </div>
+        <div id='logout-button'>
+          <GoogleLogout
+            buttonText="Logout"
+            onLogoutSuccess={this.logoutResponse}
+          >
+          </GoogleLogout>
+        </div>
       </div>
     );
   }
