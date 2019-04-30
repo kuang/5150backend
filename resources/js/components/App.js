88da1ba7c3b0643dee8e379d4918bb92c6e81680
liang195 <liang195@umn.edu>
2019-04-30 13:30:42 -0400

    add login route

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -3,79 +3,84 @@ import ReactDOM from 'react-dom';
 import Header from './Header.js'
 import Individual_resource_page from './individual_resource_page/individual_resource_page.js'
 import Individual_project_page from './individual_project_page/individual_project_page.js'
-// import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Projects_list_page from './projects_list_page/projects_list_page.js'
 import Resource_list_page from './resource_list_page/resource_list_page.js'
 import add_res_to_project from './add_resource_to_project_page/add_res_to_project.js';
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about/">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/users/">Users</Link>
-                            </li>
-                            <li>
-                                <Link to="/resource/">resource</Link>
-                            </li>
-                            <li>
-                                <Link to="/individual_resource/4">individual_resource</Link>
-                            </li>
-                            <li>
-                                <Link to="/individual_project/1">projects</Link>
-                            </li>
-                            <li>
-                                <Link to="/projects_list/">List of projects</Link>
-                            </li>
-                            <li>
-                                <Link to="/add_res_to_project/1">add_resource_to_project</Link>
-                            </li>
-                        </ul>
-                    </nav>
+import Login from './Login.js'
 
-                    <Route path="/" exact component={Index} />
-                    <Route path="/about/" component={About} />
-                    <Route path="/users/" component={Users} />
-                    <Route path="/resource/" component={Resource_list_page} />
-                    <Route
-                        path="/individual_resource/:resourceID"
-                        component={Individual_resource_page}
-                    />
-                    <Route
-                        path="/individual_project/:projectID"
-                        component={Individual_project_page}
-                    />
-                    <Route path="/projects_list/" component={Projects_list_page} />
-                    <Route path="/add_res_to_project/:projectID" component = {add_res_to_project} />
-                </div>
-            </Router>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+              <li>
+                <Link to="/resource/">resource</Link>
+              </li>
+              <li>
+                <Link to="/individual_resource/4">individual_resource</Link>
+              </li>
+              <li>
+                <Link to="/individual_project/1">projects</Link>
+              </li>
+              <li>
+                <Link to="/projects_list/">List of projects</Link>
+              </li>
+              <li>
+                <Link to="/add_res_to_project/1">add_resource_to_project</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/login" component={Login} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+          <Route path="/resource/" component={Resource_list_page} />
+          <Route
+            path="/individual_resource/:resourceID"
+            component={Individual_resource_page}
+          />
+          <Route
+            path="/individual_project/:projectID"
+            component={Individual_project_page}
+          />
+          <Route path="/projects_list/" component={Projects_list_page} />
+          <Route path="/add_res_to_project/:projectID" component = {add_res_to_project} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 function Index() {
-    return <h2>Home</h2>;
+  return <h2>Home</h2>;
 }
 
 function About() {
-    return <h2>About</h2>;
+  return <h2>About</h2>;
 }
 
 function Users() {
-    return <h2>Users</h2>;
+  return <h2>Users</h2>;
 }
 
 export default App;
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 }
--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
+import GoogleLogout from 'react-google-login';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      direct: false
+    }
+  }
 
+  render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
+    return (
+      <GoogleLogin
+        clientId="795086897508-p73emkkcd287sf6e4nm8jgb45susbcg1.apps.googleusercontent.com"
+        buttonText="Login With Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+    );
+  }
 }
 
 export default Login
+
+// if (document.getElementById('login')) {
+// 	ReactDOM.render(<Login />, document.getElementById('Login'));
+// }
